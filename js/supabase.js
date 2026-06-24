67ee647ae662820fd52b0a10a53f123216f7d2f6
denismerivera0894-hub <denis.merivera0894@gmail.com>
2026-06-23 23:11:35 -0600

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,5 +1,5 @@
 
-const SUPABASE_URL = "https://tzxqxtqwyslcenwfdlno.supabase.co/rest/v1/";
+const SUPABASE_URL = "https://tzxqxtqwyslcenwfdlno.supabase.co";
 const SUPABASE_KEY = "sb_publishable_lFvK2K71UPHS8w0mfXS1Jw_AUrwkSdF";
 
 const supabaseClient = supabase.createClient(
