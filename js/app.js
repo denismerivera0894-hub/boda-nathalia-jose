2dc17360720e765e94c19c4eacacbcda63f19bb1
denismerivera0894-hub <denis.merivera0894@gmail.com>
2026-06-23 19:45:57 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,31 @@
 
+const fechaBoda = new Date("August 14, 2026 17:00:00").getTime();
+
+setInterval(() => {
+
+    const ahora = new Date().getTime();
+    const diferencia = fechaBoda - ahora;
+
+    const dias = Math.floor(diferencia / (1000 * 60 * 60 * 24));
+
+    const horas = Math.floor(
+        (diferencia % (1000 * 60 * 60 * 24))
+        / (1000 * 60 * 60)
+    );
+
+    const minutos = Math.floor(
+        (diferencia % (1000 * 60 * 60))
+        / (1000 * 60)
+    );
+
+    const segundos = Math.floor(
+        (diferencia % (1000 * 60))
+        / 1000
+    );
+
+    document.getElementById("dias").textContent = dias;
+    document.getElementById("horas").textContent = horas;
+    document.getElementById("minutos").textContent = minutos;
+    document.getElementById("segundos").textContent = segundos;
+
+},1000);
