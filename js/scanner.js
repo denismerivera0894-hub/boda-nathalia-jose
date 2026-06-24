223134e270c784de755012e8f5d562a2dad639f8
denismerivera0894-hub <denis.merivera0894@gmail.com>
2026-06-24 13:46:16 -0600

    Create scanner.js

---
+++ js/scanner.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://tzxqxtqwyslcenwfdlno.supabase.co";
+const SUPABASE_KEY = "sb_publishable_lFvK2K71UPHS8w0mfXS1Jw_AUrwkSdF";
+
+const supabaseClient = supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
